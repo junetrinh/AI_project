adf7aece5ee9db18dbf5d5d0cc9e437031584790
junetrinh <xtrinh@student.unimelb.edu.au>
2022-05-10 16:00:18 +1000

    the game logic is implement properly

---
+++ Hello_World/environment.py
@@ -0,0 +1,121 @@
+from numpy import zeros, array, roll, vectorize
+class Environment:
+    """
+        This is data of the board
+    """
+
+    def __init__(self, board_size, taken=False):
+        """
+            complexity of create n^2
+        """
+        self.board_size = board_size
+        self._taken = dict()
+        self._available = set()
+        self._captureCount = 0
+        self._captureMove = []
+
+        
+        for n in range(board_size):
+            for q in range(board_size):
+
+                if( taken != False):
+                    if((n,q) in taken):
+                        self._taken[(n, q)] = taken[(n, q)]
+                    else:
+                        self._available.add((n, q))
+                
+                else:
+                    self._available.add((n, q))
+    
+    def getValidMove(self):
+        return list(self._available)
+
+
+    def place(self, position, token_label):
+        """
+            Assume that position adding is valid
+
+            Arguments:
+                position - Tuple(n, q)
+                token_label - character in ["red", "blue"]
+        """
+        self._available.remove(position)
+        self._taken[position] = token_label
+        self._checkAdjust4Capture(position)
+
+    
+    def steal(self):
+        """
+            Assume that position is valid
+        """
+        # add the token of player who stole his/her opponent move
+        # steal only occur in first move
+        prev_move = self._taken.keys()[0]
+
+        self._available.remove((prev_move[1], prev_move[0]))
+        self._taken[(prev_move[1], prev_move[0])] = "red" if(self._taken[prev_move] == "blue") else "blue"
+        
+        self._taken.pop(prev_move)
+        self._available.add(prev_move)
+
+    
+    def inside_bounds(self, coord):
+        """
+        REFERENC: code originate from the referee module
+        True iff coord inside board bounds.
+        """
+        r, q = coord
+        return r >= 0 and r < self.board_size and q >= 0 and q < self.board_size
+
+    def _checkAdjust4Capture(self, lastMove):
+        # reference: code from reference: pre-compile pattern of capture
+        # Check each capture pattern intersecting with coord
+        opp_type = self._taken[lastMove]
+        mid_type = "red" if opp_type == "blue" else "blue"
+        captured = set()
+        # print("TYPE::::")
+        # print(opp_type)
+        # print(mid_type)
+        # Check each capture pattern intersecting with coord
+        for pattern in _CAPTURE_PATTERNS:
+            coords = [_ADD(lastMove, s) for s in pattern]
+            
+            # No point checking if any coord is outside the board!
+            if all(map(self.inside_bounds, coords)):
+                # if(self._type == "blue"):
+                #     print("++")
+                #     print(coords)
+                try:
+                    tokens = [self._taken[coord] for coord in coords]
+                    if tokens == [opp_type, mid_type, mid_type]:
+                        # Capturing has to be deferred in case of overlaps
+                        # Both mid cell tokens should be captured
+                        captured.update(coords[1:])
+                        print("+++++++++++++++++++++++++++++++++++++++++++++++")
+                except KeyError:
+                    continue
+        for position in captured:
+            self._taken.pop(position)
+            self._available.add(position)
+            self._captureCount += 1
+            self._captureMove.append(position)
+
+
+        return list(captured)
+
+                 
+            
+    
+        
+# code that originate from the referee
+# Utility function to add two coord tuples
+_ADD = lambda a, b: (a[0] + b[0], a[1] + b[1])
+
+# Neighbour hex steps in clockwise order
+_HEX_STEPS = array([(1, -1), (1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1)], 
+    dtype="i,i")
+
+_CAPTURE_PATTERNS = [[_ADD(n1, n2), n1, n2] 
+    for n1, n2 in 
+        list(zip(_HEX_STEPS, roll(_HEX_STEPS, 1))) + 
+        list(zip(_HEX_STEPS, roll(_HEX_STEPS, 2)))]
---
+++ Hello_World/player.py
@@ -0,0 +1,68 @@
+from Hello_World.environment import *
+import random
+
+class Player:
+
+    
+    def __init__(self, player, n):
+        """
+            Called once at the beginning of a game to initialise this player.
+        Set up an internal representation of the game state.
+
+            The parameter player is the string "red" if your player will
+        play as Red, or the string "blue" if your player will play
+        as Blue.
+
+        Arguments:
+            player -- String in ["red", :blue"] represent the side + colour of the player
+            n -- Integer that is the size of the board being use
+        """
+        self._environment = Environment(n)
+        self._type = player
+        print(player)
+
+
+       # initiate the state of the game
+
+    """
+            Called at the beginning of your turn. Based on the current state
+        of the game, select an action to play.
+            ("STEAL") - a player can steal the token that the other player place last round as their own (swap color)
+            ("PLACE", r, q) - the capture + placing token on board
+    """
+    def action(self):
+        # for now just select a random move from _available list
+
+        
+        valid_move = self._environment.getValidMove()
+        # print(valid_move)
+        move = valid_move[random.randint(0, len(valid_move) - 1)]
+        if(len(self._environment._taken) == 0 and move == (2,2)):
+            move = (2, 1)
+        return ('PLACE', int(move[0]), int(move[1]))
+    
+    """
+            Called at the end of each player's turn to inform this player of 
+        their chosen action. Update your internal representation of the 
+        game state based on this. The parameter action is the chosen 
+        action itself. 
+        
+            Note: At the end of your player's turn, the action parameter is
+        the same as what your player returned from the action method
+        above. However, the referee has validated it at this point.
+
+        Arguments:
+            player -- String in ["red", :blue"] represent the side + colour of the player
+            action -- the action of either player label by the "player" attribute
+    """
+    def turn(self, player, action):
+        if(action[0] == "PLACE"):
+            self._environment.place((action[1], action[2]), player)
+        else:
+            self._environment.steal()
+        
+        # print("\n=====================================================")
+        # print(self._environment._taken)
+        # print("Capture move:" + str(self._environment._captureCount))
+        # print(self._environment._captureMove)
+        # print("=====================================================\n")
